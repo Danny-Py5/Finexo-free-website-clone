9ce0c1387bda92f8d21885857f1d78b5224a5237
DannyPY <olatundedaniel943@gmail.com>
2024-12-21 03:41:31 -0800

    working on scrolling imgs in a contatiner

--- index.js
+++ index.js
@@ -26,3 +26,46 @@ window.addEventListener('resize', (e) => {
         isToggled = !isToggled;
     }
 });
+
+
+
+const scroller = document.querySelector('.testimonial__scroller');
+const cards = document.querySelectorAll('.testimonial__card');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let isCliked = false;
+let mouseClickStartPosition = undefined;
+let scrollLeft = undefined;
+
+document.addEventListener('mousedown', (e) => {
+    mouseClickStartPosition = e.pageX - scroller.offsetLeft;
+    scrollLeft = scroller.scrollLeft;
+    isCliked = true;
+})
+document.addEventListener('mousemove', (e) => {
+    if (!isCliked) return; 
+    e.preventDefault();
+    const mouseMovedDistance = e.pageX - mouseClickStartPosition;
+    scroller.scrollLeft = scrollLeft - mouseMovedDistance;
+});
+document.addEventListener('mouseup', (e) => {
+    isCliked = false;
+});
+document.addEventListener('mouseout', (e) => {
+    isCliked = false;
+});
+
+let currentIndex = 0;
+
+next.addEventListener('click', () => {
+    if (currentIndex < cards.length - 1){
+        currentIndex++;
+        cards[currentIndex].scrollIntoView({
+            behavior: 'smooth', block: 'nearest', inline: 'start' 
+        });
+    }
+});
+
+
+
